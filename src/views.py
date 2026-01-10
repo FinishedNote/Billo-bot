244ac711d1cab7ed34586b334b5145b86726474c
Miroir Raphaël <miroir.raphael07@gmail.com>
2026-01-10 13:45:26 +0100

    fix: int conversion error

--- src/views.py
+++ src/views.py
@@ -141,7 +141,11 @@ class DynamicModal(ui.Modal):
             if self.template_name == "moncler.html" or self.template_name == "dior.html":
                 data["order_total"] = str(int(data["price"]) + 15)
             elif self.template_name == "stockx.html":
-                data["order_total"] = (float(data["price"]) + float(data["processing_fee"] + float(data["shipping"])))
+                data["order_total"] = str(
+                float(data["price"]) + 
+                float(data["processing_fee"]) + 
+                float(data["shipping"])
+            )
             
             env = Environment(loader=FileSystemLoader('src/templates'))
             template = env.get_template(self.template_name)
